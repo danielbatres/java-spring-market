b6856ebd6c9c8bed21ddcce0664d4ef86d509f32
danielbatres <danielbatresuser@gmail.com>
2023-11-19 13:07:39 -0600

    Using the CrudRepository interface

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -1,5 +1,6 @@
 package com.platzi.market;
 
+import com.platzi.market.persistence.ProductoRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
